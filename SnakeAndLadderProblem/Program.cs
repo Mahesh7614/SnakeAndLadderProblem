aa734452150d8660d02fee87429d8ebaa5b18b3f
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-01 22:20:33 +0530

    UC1 Single Player

---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadderProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************Welcome To Sanke and Ladder Game*************************");
+
+            SnakeLadderGame game = new SnakeLadderGame();
+            game.SnakeLadder();
+        }
+    }
+}
